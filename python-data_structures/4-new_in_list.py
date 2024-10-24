4d7c34ce0c43319c23f7f2fffb2ea07324449081
Mbonyumugisha <p.mbonyumug@alustudent.com>
2024-10-24 06:33:35 -0700

    commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Create a copy of the original list
     new_list = my_list[:]
-    
-    # Check if idx is within the valid range
     if 0 <= idx < len(my_list):
         new_list[idx] = element
-    
-    # Return the new list, whether modified or not
     return new_list
+
