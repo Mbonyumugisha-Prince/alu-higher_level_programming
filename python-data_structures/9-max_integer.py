e47798df026307a9ca5c1c6dccd1c6b7a59ae3c6
Mbonyumugisha <p.mbonyumug@alustudent.com>
2024-10-24 07:29:26 -0700

    commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    max_val = my_list[0]
-    for num in my_list:
-        if num > max_val:
-            max_val = num
-    return max_val
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
