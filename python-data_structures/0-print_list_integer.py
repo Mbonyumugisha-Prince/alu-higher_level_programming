2e48a48815e642608d89a72cb9e8941a69ab21ae
Mbonyumugisha <p.mbonyumug@alustudent.com>
2024-10-24 04:51:17 -0700

    commit

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    if matrix:
-        result = [list(map(lambda column: column ** 2, row)) for row in matrix]
-        return result
-    else:
-        return
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
+
