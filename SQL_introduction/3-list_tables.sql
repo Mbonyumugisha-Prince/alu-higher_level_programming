a49abb281cc4140f5c9c0b687bad5d5d6c37fd42
prince <p.mbonyumug@alustudent.com>
2024-11-13 12:40:04 +0200

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,5 @@
--- Content: List all tables in the current database
-SHOW TABLES;
+-- This script lists all the tables in the specified database
+USE INFORMATION_SCHEMA;
+SELECT table_name
+FROM tables
+WHERE table_schema = DATABASE();
